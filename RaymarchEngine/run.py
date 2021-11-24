82cf023b30a14c69fcd89d031b0e34e3d0d2ec9e
gresm <>
2021-11-24 18:38:20 +0100

    first step for greatmess

---
+++ RaymarchEngine/run.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+def generate_rays():
+    pass
---
+++ RaymarchEngine/shapes.py
@@ -0,0 +1,7 @@
+import math
+
+
+def circle(ray_pos, circle_size):
+    def algorithm():
+        return math.sqrt(ray_pos) - circle_size
+    return algorithm
