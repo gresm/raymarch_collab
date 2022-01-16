2d9df8740cad5a92673fe32eddadee3a680fd909
gresm <78505251+gresm@users.noreply.github.com>
2022-01-16 09:13:05 +0100

    + game_scenes.py ~ game_scenes/ -> game ~ main.py

---
+++ game_scenes.py
@@ -0,0 +1,5 @@
+from game import BaseScene
+
+
+class MainMenuScene(BaseScene):
+    pass
---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame as pg
+
+from game import scene_manager as scenes
+from game import size, game
+from game_scenes import MainMenuScene
+
+scenes.spawn_scene(MainMenuScene)
+
+
+@game.frame
+def frame(window, delta_time):
+    for ev in pg.event.get():
+        if ev.type == pg.QUIT:
+            game.stop()
+        else:
+            scenes.handle_events(ev)
+
+    scenes.update()
+
+    window.fill((0, 0, 0))
+
+    scenes.draw(window)
+
+    pg.display.update()
+
+
+if __name__ == '__main__':
+    game.run()
